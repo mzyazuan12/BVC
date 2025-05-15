ff703f600b4c8a4c2ba81182bbaa6d51e3febd05
Zayan <142456754+mzyazuan12@users.noreply.github.com>
2025-05-14 23:36:47 -0500

    Update z.py

--- z.py
+++ z.py
@@ -1,11 +1,1 @@
-import hashlib, pyotp
 
-shared_secret = "mohammedzayantariq12@gmail.comHENNGECHALLENGE004"
-totp = pyotp.TOTP(
-    shared_secret,
-    digits=10,
-    digest=hashlib.sha512,
-    interval=30
-)
-code = totp.now()
-print(code)
